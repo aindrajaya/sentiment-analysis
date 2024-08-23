347ff3a2daecd4479c87fbb71765d7a7017399b6
luqmanamirullah <amirullahluqman@gmail.com>
2024-08-23 14:04:27 +0700

    feat: new server using typscript

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+// dependencies / libraries
+import express, { NextFunction, Request, Response } from "express";
+import { configServer } from "./configs/server.config.js";
+
+// middlewares
+import apiKey from "./middlewares/apiKey.middleware.js";
+
+// routers
+import sentimentRouter from "./modules/sentiment/index.js";
+import aiScraperRouter from "./modules/ai-scraper/index.js";
+
+// configs
+import * as config from "./configs/general.config.js";
+
+const port = config.port || 3000;
+const app = express();
+configServer(app);
+
+// endpoint
+app.get("/", (req: Request, res: Response) => {
+  res.send("MR Scraper Sentiment Analysis API");
+});
+app.use("/detect-sentiment", apiKey, sentimentRouter);
+app.use("/ai-scraper/v1", apiKey, aiScraperRouter);
+
+/* Error handler */
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+});
+
+// logger
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
