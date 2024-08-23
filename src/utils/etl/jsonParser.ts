e03832ba34e3fd2c5734c5f846a55f056281d542
luqmanamirullah <amirullahluqman@gmail.com>
2024-08-23 14:03:32 +0700

    feat: json parser

---
+++ src/utils/etl/jsonParser.ts
@@ -0,0 +1,18 @@
+export default function jsonParser(text: string): string | null {
+  // EXPECTED PARAMS is string that start with ```json and end with ```
+  const start = text.indexOf("```json");
+  const end = text.indexOf("```", start + 1);
+
+  if (start === -1 || end === -1) {
+    console.error("Failed to find the json snippet");
+    return null;
+  }
+
+  const jsonString = text.slice(start + 7, end).trim();
+  try {
+    return JSON.parse(jsonString);
+  } catch (e) {
+    console.error("Failed while parsing the text to json", e);
+    return null;
+  }
+}
