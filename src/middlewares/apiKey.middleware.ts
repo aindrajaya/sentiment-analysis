8cc9a3d27375e4cdb2b7cd3a8770a04a346767b7
luqmanamirullah <amirullahluqman@gmail.com>
2024-08-27 17:17:52 +0700

    feat: new validation for validate request api key

--- src/middlewares/apiKey.middleware.ts
+++ src/middlewares/apiKey.middleware.ts
@@ -2,9 +2,37 @@ import { errorResponse } from "../utils/helper.util.js";
 import * as config from "../configs/general.config.js";
 import type { NextFunction, Request, Response } from "express";
 
+const apiUrl = config.platformApiUrl + "/account";
+async function validateToken(token: string) {
+  console.log("Validate API URL:", apiUrl);
+  const bearer = "Bearer " + token;
+  console.log("Bearer:", bearer);
+
+  const res = await fetch(apiUrl, {
+    headers: {
+      Authorization: bearer,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    console.log("response:", res);
+    return false;
+  }
+
+  const data = await res.json();
+  console.log("result:", JSON.stringify(data, null, 2));
+
+  return true;
+}
+
 const apiKey = async (req: Request, res: Response, next: NextFunction) => {
-  const apiKey = req.headers["x-api-key"];
-  if (apiKey === config.apiKey) {
+  const apiKey = req.headers["x-api-key"] as string;
+  const validation = await validateToken(apiKey);
+  if (validation) {
+    return next();
+  } else if (apiKey === config.apiKey) {
     return next();
   }
   return errorResponse(res, "Invalid API Key", null, 401);
