3979dfff5568342a4c2501c81d319e0797ae7e12
Najwan Aribena <aribenanajwan@gmail.com>
2024-08-27 09:51:58 +0700

    feat: return openai token usage

--- src/modules/ai-scraper/aiScrpaer.service.ts
+++ src/modules/ai-scraper/aiScrpaer.service.ts
@@ -75,9 +75,9 @@ export async function askAi(req: Request, res: Response) {
     // STEP 3: [x] ============== Chat AI ===============
     console.log("Chat AI...");
     const context = customFormatMarkdownDocAsString(rerankResult);
-    const tokens = countTokens(context);
-    console.log(`\n=======\nTokens usage: ${tokens}\n=======\n`);
     const input = `Text:${context}\n\n\nI need ${task} from the above data".\n IMPORTANT!! return the answer with json format \n eg. \`\`\`json\n JSON_HERE \`\`\` `;
+    const inputTokens = countTokens(context + input);
+    console.log(`\n=======\nInput token usage: ${inputTokens}\n=======\n`);
 
     const chatModel = new ChatOpenAI({
       model: "gpt-4o",
@@ -93,12 +93,19 @@ export async function askAi(req: Request, res: Response) {
     // STEP 4: [x] ============== Output Parser ===============
     console.log("Parsing into JSON format...");
     const output = jsonParser(result.content.toString());
+    const outputTokens = countTokens(result.content.toString());
+    console.log(`\n=======\nOutput tokens usage: ${outputTokens}\n=======\n`);
     console.log("Output", output);
     if (output) {
       return successResponse(
         res,
         "AI Scraper completed successfully",
-        { json: output, task },
+        { 
+            json: output,
+            task,
+            inputTokens,
+            outputTokens 
+        },
         200,
       );
     }
