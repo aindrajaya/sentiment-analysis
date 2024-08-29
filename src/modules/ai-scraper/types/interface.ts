7b6c3baf01e9e1de8556fa07c16491e266d4a62e
luqmanamirullah <amirullahluqman@gmail.com>
2024-08-29 18:48:43 +0700

    feat: setup new ai scraper with conversation payload & response type

--- src/modules/ai-scraper/types/interface.ts
+++ src/modules/ai-scraper/types/interface.ts
@@ -3,6 +3,17 @@ export interface AiScraperBodyRequest {
   task: string;
 }
 
+export interface AiScraperV2BodyRequest {
+  markdown: string;
+  task: string;
+  sessionId: string;
+}
+
+export interface AiScraperBodyResponse {
+  desc: string;
+  result: string;
+}
+
 export interface AiScraperBodyResponse {
   task: string;
   json: string;
