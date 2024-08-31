d4f9659c73b84b6659092a32273d03f9ee747cb9
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-01 00:30:56 +0700

    feat: search web content tool

--- src/utils/langchain/tools/searchWebContent.ts
+++ src/utils/langchain/tools/searchWebContent.ts
@@ -1,18 +1,20 @@
+import "dotenv/config";
 import { Tool, ToolParams } from "langchain/tools";
 import { Document as ChainDoc } from "@langchain/core/documents";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { FaissStore } from "@langchain/community/vectorstores/faiss";
 import { CohereRerank } from "@langchain/cohere";
-import "dotenv/config";
-export class RetrieverTool extends Tool {
+import { customFormatMarkdownDocAsString } from "../../etl/markdown.js";
+export class SearchWebContentTool extends Tool {
   data: ChainDoc[] = [];
   static lc_name() {
-    return "RetrieverTool";
+    return "SearchWebContentTool";
   }
 
-  name = "retriever-tool";
+  name = "search-web-content-tool";
 
-  description = "Tool used when you need to retrieve a data";
+  description =
+    "Tool used when you need to search related web content to answer user question.";
 
   constructor(data: ChainDoc[], config?: ToolParams) {
     super(config);
@@ -42,6 +44,8 @@ export class RetrieverTool extends Tool {
     const rerankResult = rerankedDocuments.map((r) => this.data[r.index]);
     console.log("Rerank", rerankResult);
 
-    return rerankResult;
+    const context = customFormatMarkdownDocAsString(rerankResult);
+
+    return context;
   }
 }
