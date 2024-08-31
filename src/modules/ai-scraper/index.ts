8e1db79695ebb0624242f86e369d01595a21dee4
luqmanamirullah <amirullahluqman@gmail.com>
2024-08-31 12:25:16 +0700

    feat: ai scraper v2 endpoint

--- src/modules/ai-scraper/index.ts
+++ src/modules/ai-scraper/index.ts
@@ -1,8 +1,9 @@
 import * as express from "express";
 import { aiScraperValidation } from "./aiScraper.validation.js";
-import { askAi } from "./aiScrpaer.service.js";
+import { askAi, askAiV2 } from "./aiScrpaer.service.js";
 
-const sentimentRouter = express.Router();
-sentimentRouter.post("/", aiScraperValidation, askAi);
+const aiRouter = express.Router();
+aiRouter.post("/v1", aiScraperValidation, askAi);
+aiRouter.post("/v2", askAiV2);
 
-export default sentimentRouter;
+export default aiRouter;
