253c3ec1d9d920b6f4f0a506b76640ede76c674e
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-01 00:30:36 +0700

    feat: v2 & identify endpoint

--- src/modules/ai-scraper/index.ts
+++ src/modules/ai-scraper/index.ts
@@ -1,18 +1,14 @@
 import * as express from "express";
-import { aiScraperValidation } from "./aiScraper.validation.js";
-import { askAi, askAiV2 } from "./aiScrpaer.service.js";
+import {
+  aiIdentifierValidation,
+  aiScraperV2Validation,
+  aiScraperValidation,
+} from "./aiScraper.validation.js";
+import { askAi, askAiV2, identifyContent } from "./aiScrpaer.service.js";
 
 const aiRouter = express.Router();
 aiRouter.post("/v1", aiScraperValidation, askAi);
-aiRouter.post("/v2", askAiV2);
-import {
-  aiIdentifierValidation,
-  aiScraperValidation
-} from "./aiScraper.validation.js";
-import { askAi, identifyContent } from "./aiScrpaer.service.js";
-
-const sentimentRouter = express.Router();
-sentimentRouter.post("/", aiScraperValidation, askAi);
-sentimentRouter.post("/identify", aiIdentifierValidation, identifyContent);
+aiRouter.post("/v2", aiScraperV2Validation, askAiV2);
+aiRouter.post("/identify", aiIdentifierValidation, identifyContent);
 
 export default aiRouter;
