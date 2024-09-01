9ebe8ea53880ea198a3ef69127eb77d53eaa1785
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-02 01:17:06 +0700

    feat: socket server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // dependencies / libraries
 import express, { NextFunction, Request, Response } from "express";
-import { configServer } from "./configs/server.config.js";
-
+import { Server } from "socket.io";
+import { createServer } from "http";
+import path from "path";
+import { fileURLToPath } from "url";
 // middlewares
 import apiKey from "./middlewares/apiKey.middleware.js";
 
@@ -11,11 +13,18 @@ import aiScraperRouter from "./modules/ai-scraper/index.js";
 
 // configs
 import * as config from "./configs/general.config.js";
+import { configServer } from "./configs/server.config.js";
 import { connectToMongo, db } from "./configs/databases/mongodb.db.js";
+import socketServer from "./socketServer.js";
+import { join } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 async function main() {
   const port = config.port || 3000;
   const app = express();
+  const server = createServer(app);
+  const io = new Server(server);
   await connectToMongo();
   if (!db) {
     console.log("MongoDB connection failed");
@@ -23,9 +32,21 @@ async function main() {
   }
   configServer(app);
 
+  /*
+   * Web Socket Server
+   */
+  socketServer(io);
+
+  /*
+   * REST API
+   */
   // endpoint
   app.get("/", (req: Request, res: Response) => {
     res.send("MR Scraper Sentiment Analysis API");
+  });
+
+  app.get("/test", (req: Request, res: Response) => {
+    res.sendFile(join(__dirname, "index.html"));
   });
   app.use("/detect-sentiment", apiKey, sentimentRouter);
   app.use("/ai-scraper", apiKey, aiScraperRouter);
@@ -36,7 +57,7 @@ async function main() {
   });
 
   // logger
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
   });
 }
---
+++ src/socketServer.ts
@@ -0,0 +1,39 @@
+import { Server } from "socket.io";
+import { AiScraperV2BodyRequest } from "./modules/ai-scraper/types/interface.js";
+import { aiScraperV2Validation } from "./modules/ai-scraper/aiScraper.validation.js";
+import socketApiKey from "./middlewares/socketApiKey.middleware.js";
+import { askAiV2 } from "./modules/ai-scraper/aiScrpaer.service.js";
+
+export default function socketServer(io: Server) {
+  io.use((socket, next) =>
+    socketApiKey(socket.handshake.auth.apiKey, (status) => {
+      if (!status) {
+        io.to(socket.id).emit("error", "Invalid API Key");
+        // close the connection
+        socket.disconnect();
+      } else {
+        console.log("Socket API Key is valid");
+        next();
+      }
+    }),
+  );
+
+  io.on("connection", (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+    socket.on("disconnect", () => {
+      console.log(`Client disconnected: ${socket.id}`);
+    });
+
+    socket.on("ask-ai-v2", async (payload: AiScraperV2BodyRequest) => {
+      console.log("ask-ai-v2", payload);
+      const { status, message } = aiScraperV2Validation(payload);
+      if (!status) {
+        io.to(socket.id).emit("conversation-error", message);
+      } else {
+        await askAiV2(payload, (res) => {
+          io.to(socket.id).emit("answer-ai-v2", res);
+        });
+      }
+    });
+  });
+}
