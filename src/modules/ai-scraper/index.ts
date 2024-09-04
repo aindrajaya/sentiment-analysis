20db493b07597480c8b31ce2239aee76f7d273e8
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-04 12:08:25 +0700

    feat: ai scraper v2 api

--- src/modules/ai-scraper/index.ts
+++ src/modules/ai-scraper/index.ts
@@ -5,17 +5,30 @@ import {
   aiScraperV2GetChatHistoryValidation,
   aiScraperV2GetSessionsValidation,
   aiScraperValidation,
+  apiAiScraperV2Validation,
 } from "./aiScraper.validation.js";
 import {
   askAi,
+  askAiV2,
   getChatHistory,
   getSessions,
   identifyContent,
   saveFinalAnswer,
 } from "./aiScrpaer.service.js";
+import { successResponse } from "../../utils/helper.util.js";
 
 const aiRouter = express.Router();
 aiRouter.post("/v1", aiScraperValidation, askAi);
+aiRouter.post(
+  "/v2",
+  apiAiScraperV2Validation,
+  (req: express.Request, res: express.Response) =>
+    askAiV2(
+      req.body,
+      (result) => successResponse(res, "answered", result, 200),
+      false,
+    ),
+);
 aiRouter.post("/v2/save-result", aiFinalAnswerValidation, saveFinalAnswer);
 aiRouter.get(
   "/v2/chat-history/:userId",
