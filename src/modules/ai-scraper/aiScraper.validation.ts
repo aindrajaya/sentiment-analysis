5911b7259ad9062fffd378d5c4840e549fff9863
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-16 13:23:09 +0700

    feat: json schema validation

--- src/modules/ai-scraper/aiScraper.validation.ts
+++ src/modules/ai-scraper/aiScraper.validation.ts
@@ -102,8 +102,217 @@ const AiIdentifierSchema = z.object({
   }),
 });
 
+const propertiesSchema = z.record(
+  z.string().nonempty({ message: "Key is required" }),
+  z
+    .object({
+      type: z.string({
+        required_error: "Type is required",
+      }),
+      description: z
+        .string({
+          required_error: "Description is required",
+        })
+        .optional(),
+      properties: z
+        .record(
+          z.string().nonempty({ message: "Key is required" }),
+          z.object({
+            type: z.string({
+              required_error: "Type is required",
+            }),
+            description: z.string({
+              required_error: "Description is required",
+            }),
+          }),
+        )
+        .optional(),
+      items: z
+        .object({
+          type: z.string({
+            required_error: "Type is required",
+          }),
+          description: z
+            .string({
+              required_error: "Description is required",
+            })
+            .optional(),
+          properties: z
+            .record(
+              z.string().nonempty({ message: "Key is required" }),
+              z.object({
+                type: z.string({
+                  required_error: "Type is required",
+                }),
+                description: z.string({
+                  required_error: "Description is required",
+                }),
+              }),
+            )
+            .optional(),
+          required: z.array(z.string()).optional(),
+        })
+        .optional(),
+      required: z.array(z.string()).optional(),
+    })
+    .superRefine((value, ctx) => {
+      if (value.type === "object" && !value.properties) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Properties is required for object type",
+        });
+      } else if (value.type === "array" && !value.items) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Items is required for array type",
+        });
+      }
+
+      if (value.required && value.properties) {
+        for (const key of value.required) {
+          if (!value.properties[key]) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: `Required key ${key} is not in properties`,
+            });
+          }
+        }
+      }
+
+      if (value.items && value.items.type === "object") {
+        for (const key of Object.keys(value.items.properties!)) {
+          if (value.items.required && !value.items.required.includes(key)) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: `Key ${key} is not in required keys`,
+            });
+          }
+        }
+      }
+    }),
+);
+
+const typeSchema = z.enum(["object", "array", "string", "number", "boolean"], {
+  required_error: "Type is required",
+  invalid_type_error: "Type must be either object or array",
+});
+
+const itemsSchema = z
+  .object({
+    type: typeSchema,
+    description: z
+      .string({
+        invalid_type_error: "Description must be a string",
+      })
+      .optional(),
+    items: z
+      .object({
+        type: z.string({
+          required_error: "Type is required",
+        }),
+        description: z
+          .string({
+            required_error: "Description is required",
+          })
+          .optional(),
+        properties: propertiesSchema.optional(),
+        required: z.array(z.string()).optional(),
+      })
+      .optional(),
+    properties: propertiesSchema.optional(),
+    required: z.array(z.string()).optional(),
+  })
+  .superRefine((value, ctx) => {
+    if (value.type === "object" && !value.properties) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Properties is required for object type",
+      });
+    }
+
+    if (value.type === "array" && !value.items) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Items is required for array type",
+      });
+    }
+
+    if (value.items && value.items.type === "object") {
+      for (const key of Object.keys(value.items.properties!)) {
+        if (value.items.required && !value.items.required.includes(key)) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Key ${key} is not in required keys`,
+          });
+        }
+      }
+    }
+
+    if (value.required && value.properties) {
+      for (const key of value.required) {
+        if (!value.properties[key]) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Required key ${key} is not in properties`,
+          });
+        }
+      }
+    }
+  });
+
+const AiScraperApiSchema = z.object({
+  body: z
+    .object({
+      markdown: z.string({
+        required_error: "Markdown is required",
+      }),
+      schema: z.object({
+        type: typeSchema,
+        items: itemsSchema.optional(),
+        properties: propertiesSchema.optional(),
+        required: z.array(z.string()).optional(),
+      }),
+    })
+    .superRefine(({ markdown, schema }, ctx) => {
+      if (schema.type === "array" && !schema.items) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Items is required for array schema",
+        });
+      } else if (schema.type === "object" && !schema.properties) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Properties is required for object schema",
+        });
+      }
+
+      if (schema.required && schema.properties) {
+        for (const key of schema.required) {
+          if (!schema.properties[key]) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: `Required key ${key} is not in properties`,
+            });
+          }
+        }
+      }
+
+      if (schema.items && schema.items.type === "object") {
+        for (const key of Object.keys(schema.items.properties!)) {
+          if (schema.items.required && !schema.items.required.includes(key)) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: `Key ${key} is not in required keys`,
+            });
+          }
+        }
+      }
+    }),
+});
+
 const aiScraperValidation = validate(AiScraperSchema);
 const aiScraperV2Validation = socketValidate(AiScraperSchemaV2);
+const aiScraperApiValidation = validate(AiScraperApiSchema);
 const apiAiScraperV2Validation = validate(AiScraperSchemaV2);
 const aiIdentifierValidation = validate(AiIdentifierSchema);
 const aiScraperV2GetSessionsValidation = validate(AiScraperSchemaGetSessions);
@@ -124,4 +333,5 @@ export {
   aiScraperV2GetSessionsValidation,
   aiScraperV2GetChatHistoryValidation,
   aiScraperV2MigrateChatHistoryValidation,
+  aiScraperApiValidation,
 };
