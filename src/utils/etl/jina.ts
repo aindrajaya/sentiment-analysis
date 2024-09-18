78b01b69d9551876c268180641d51f4ecdf1c82c
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-18 16:30:17 +0700

    feat: proxy

--- src/utils/etl/jina.ts
+++ src/utils/etl/jina.ts
@@ -1,11 +1,16 @@
 import { jinaApiKey } from "../../configs/general.config.js";
+import fetch from "node-fetch";
+import { HttpsProxyAgent } from "https-proxy-agent";
 
 const extractMarkdown = async (base_url: string) => {
   const url = `https://r.jina.ai/${base_url}`;
   const data = await fetch(url, {
-    headers: {
-      Authorization: "Bearer " + jinaApiKey,
-    },
+    // headers: {
+    //   Authorization: "Bearer " + jinaApiKey,
+    // },
+    agent: new HttpsProxyAgent(
+      "http://xjrxxdsh-rotate:bs1a64qlhfnk@p.webshare.io:80",
+    ),
   })
     .then((res) => res.text())
     .catch((err) => "");
