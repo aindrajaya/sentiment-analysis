9e8a46aa3dbe49fba8c4f7de2ec2ecc1fe26cb03
luqmanamirullah <amirullahluqman@gmail.com>
2024-09-18 15:57:51 +0700

    feat: handle nested web content result out of open ai limit token

--- src/utils/helper.util.ts
+++ src/utils/helper.util.ts
@@ -60,6 +60,7 @@ async function streamAIV2Response(
   let currentStream = "";
   let isDescDone = false;
   for await (const chunk of logStream) {
+    console.log("streaming");
     if (!finalState) {
       finalState = chunk;
     } else {
